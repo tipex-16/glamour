5a8208220018be163aa17cf24554324bdb792e78
gustave <2909789120@qq.com>
2025-12-18 23:02:10 +0800

    fix: hide mailto: prefix in rendered email links

--- ansi/elements.go
+++ ansi/elements.go
@@ -291,10 +291,12 @@ func (tr *ANSIRenderer) NewElement(node ast.Node, source []byte) Element {
 				SkipHref: true,
 			}
 		} else {
+			isEmail := n.AutoLinkType == ast.AutoLinkEmail
 			renderer = &LinkElement{
 				Children: children,
 				URL:      u,
-				SkipText: n.AutoLinkType != ast.AutoLinkEmail,
+				SkipText: !isEmail, // For non-email links, skip text (show only href)
+				SkipHref: isEmail,  // For email links, skip href (hide mailto: URL)
 			}
 		}
 		return Element{Renderer: renderer}
